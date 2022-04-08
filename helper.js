14a3b1c15f10d2334b6468bdea38e857d74d779d
Carlos Bonetti <carlosbonetti.mail@gmail.com>
2022-04-08 10:15:11 -0300

    set reminder date as env variables

--- helper.js
+++ helper.js
@@ -1,8 +1,14 @@
 export const getNextReminderDate = (tz_offset = 0) => {
+  const dayOfWeek = process.env.SLACK_REMINDER_DAY_OF_WEEK;
+  const hour = process.env.SLACK_REMINDER_HOUR;
+  const minute = process.env.SLACK_REMINDER_MINUTE;
+
   const today = new Date();
   const nextThursday = new Date(today.getTime());
-  nextThursday.setDate(today.getDate() + ((4 - today.getDay() + 7) % 7));
-  nextThursday.setHours(17, 0, 0);
+  nextThursday.setDate(
+    today.getDate() + ((dayOfWeek - today.getDay() + 7) % 7)
+  );
+  nextThursday.setHours(hour, minute, 0);
   const nextThursdayDate = nextThursday.getTime() / 1000;
   return Math.round(nextThursdayDate - tz_offset);
 };
